6ed4f5429f1e4f3d2461ee6c521438ef4109c9a3
Mitar <mitar.git@tnode.com>
2023-06-21 11:44:44 +0200

    Fix races in tests.

--- internal/dinit/dinit_test.go
+++ internal/dinit/dinit_test.go
@@ -5,9 +5,11 @@ import (
 	"context"
 	"io"
 	"log"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -18,32 +20,55 @@ import (
 	"gitlab.com/tozd/dinit/internal/dinit"
 )
 
+func withLogger(t *testing.T, f func()) string {
+	t.Helper()
+	reader, writer, err := os.Pipe()
+	defer reader.Close() //nolint:staticcheck
+	// We might double close writer here, but that is OK and we ignore any error.
+	defer writer.Close()
+	require.NoError(t, err)
+	orgWriter := log.Writer()
+	log.SetOutput(writer)
+	defer func() {
+		log.SetOutput(orgWriter)
+	}()
+
+	var wg sync.WaitGroup
+	var l []byte
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		l, _ = io.ReadAll(reader)
+	}()
+	dinit.ConfigureLog("info")
+	f()
+	writer.Close()
+	wg.Wait()
+	return string(l)
+}
+
 func TestReparentingTerminate(t *testing.T) {
-	var buf bytes.Buffer
-	writer := log.Writer()
-	log.SetOutput(&buf)
-	t.Cleanup(func() {
-		log.SetOutput(writer)
+	l := withLogger(t, func() {
+		cmd := exec.Command("/bin/sleep", "infinity")
+		e := cmd.Start()
+		require.NoError(t, e)
+		t.Cleanup(func() {
+			_ = cmd.Process.Kill()
+			_, _ = cmd.Process.Wait()
+		})
+
+		// So that the command runs.
+		time.Sleep(10 * time.Millisecond)
+
+		g, ctx := errgroup.WithContext(context.Background())
+
+		dinit.ProcessPid(ctx, g, dinit.ReparentingTerminate, cmd.Process.Pid)
+
+		e = g.Wait()
+		require.NoError(t, e)
 	})
 
-	dinit.ConfigureLog("debug")
-
-	cmd := exec.Command("/bin/sleep", "infinity")
-	e := cmd.Start()
-	require.NoError(t, e)
-	t.Cleanup(func() {
-		_ = cmd.Process.Kill()
-		_, _ = cmd.Process.Wait()
-	})
-
-	g, ctx := errgroup.WithContext(context.Background())
-
-	dinit.ProcessPid(ctx, g, dinit.ReparentingTerminate, cmd.Process.Pid)
-
-	e = g.Wait()
-	require.NoError(t, e)
-
-	lines := strings.Split(buf.String(), "\n")
+	lines := strings.Split(l, "\n")
 	require.Len(t, lines, 4)
 	assert.Regexp(t, `.+Z dinit: warning: sleep/\d+: terminating reparented child process with PID \d+(: /bin/sleep infinity)?`, lines[0])
 	assert.Regexp(t, `.+Z dinit: info: sleep/\d+: sending SIGTERM to PID \d+`, lines[1])
@@ -51,42 +76,80 @@ func TestReparentingTerminate(t *testing.T) {
 	assert.Equal(t, "", lines[3])
 }
 
-func TestReparentingAdopt(t *testing.T) {
-	var buf bytes.Buffer
-	writer := log.Writer()
-	log.SetOutput(&buf)
-	t.Cleanup(func() {
-		log.SetOutput(writer)
+func TestReparentingAdoptCancel(t *testing.T) {
+	l := withLogger(t, func() {
+		cmd := exec.Command("/bin/sleep", "infinity")
+		e := cmd.Start()
+		require.NoError(t, e)
+		t.Cleanup(func() {
+			_ = cmd.Process.Kill()
+			_, _ = cmd.Process.Wait()
+		})
+
+		// So that the command runs.
+		time.Sleep(10 * time.Millisecond)
+
+		ctx, cancel := context.WithCancel(context.Background())
+
+		g, ctx := errgroup.WithContext(ctx)
+
+		dinit.ProcessPid(ctx, g, dinit.ReparentingAdopt, cmd.Process.Pid)
+
+		cancel()
+
+		e = g.Wait()
+		require.ErrorAs(t, e, &context.Canceled)
 	})
 
-	dinit.ConfigureLog("debug")
-
-	cmd := exec.Command("/bin/sleep", "infinity")
-	e := cmd.Start()
-	require.NoError(t, e)
-	t.Cleanup(func() {
-		_ = cmd.Process.Kill()
-		_, _ = cmd.Process.Wait()
-	})
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	g, ctx := errgroup.WithContext(ctx)
-
-	dinit.ProcessPid(ctx, g, dinit.ReparentingAdopt, cmd.Process.Pid)
-
-	cancel()
-
-	e = g.Wait()
-	require.ErrorAs(t, e, &context.Canceled)
-
-	lines := strings.Split(buf.String(), "\n")
+	lines := strings.Split(l, "\n")
 	require.Len(t, lines, 5)
 	assert.Regexp(t, `.+Z dinit: warning: sleep/\d+: adopting reparented child process with PID \d+(: /bin/sleep infinity)?`, lines[0])
 	assert.Regexp(t, `.+Z dinit: info: sleep/\d+: finishing`, lines[1])
 	assert.Regexp(t, `.+Z dinit: info: sleep/\d+: sending SIGTERM to PID \d+`, lines[2])
 	assert.Regexp(t, `.+Z dinit: info: sleep/\d+: PID \d+ finished with signal 15`, lines[3])
 	assert.Equal(t, "", lines[4])
+}
+
+func TestReparentingAdoptFinish(t *testing.T) {
+	l := withLogger(t, func() {
+		stdin, stdinWriter, e := os.Pipe()
+		require.NoError(t, e)
+		t.Cleanup(func() {
+			_ = stdin.Close()
+			_ = stdinWriter.Close()
+		})
+
+		cmd := exec.Command("/bin/bash", "-c", "read; echo end")
+		cmd.Stdin = stdin
+		e = cmd.Start()
+		require.NoError(t, e)
+		t.Cleanup(func() {
+			_ = cmd.Process.Kill()
+			_, _ = cmd.Process.Wait()
+		})
+
+		// So that the command runs.
+		time.Sleep(10 * time.Millisecond)
+
+		g, ctx := errgroup.WithContext(context.Background())
+
+		dinit.ProcessPid(ctx, g, dinit.ReparentingAdopt, cmd.Process.Pid)
+
+		// Time to adopt.
+		time.Sleep(10 * time.Millisecond)
+
+		_, _ = stdinWriter.WriteString("\n")
+
+		e = g.Wait()
+		require.NoError(t, e)
+	})
+
+	lines := strings.Split(l, "\n")
+	require.Len(t, lines, 4)
+	assert.Regexp(t, `.+Z dinit: warning: bash/\d+: adopting reparented child process with PID \d+(: /bin/bash -c read; echo end)?`, lines[0])
+	assert.Regexp(t, `.+Z dinit: warning: bash/\d+: not JSON stdout: end`, lines[1])
+	assert.Regexp(t, `.+Z dinit: info: bash/\d+: PID \d+ finished with status 0`, lines[2])
+	assert.Equal(t, "", lines[3])
 }
 
 func TestGetProcessInfo(t *testing.T) {
@@ -149,30 +212,23 @@ func TestIsZombie(t *testing.T) {
 }
 
 func TestRedirectJSON(t *testing.T) {
-	var buf bytes.Buffer
-	writer := log.Writer()
-	log.SetOutput(&buf)
-	t.Cleanup(func() {
-		log.SetOutput(writer)
+	l := withLogger(t, func() {
+		var in bytes.Buffer
+		var out bytes.Buffer
+		in.WriteString(`{"test":"foo"}`)
+		dinit.RedirectJSON("run", "test", []byte("test"), io.NopCloser(&in), &out)
+
+		assert.Regexp(t, `\{"test":"foo","service":test,"stage":"run","logged":".+"\}`, out.String())
 	})
+	assert.Equal(t, "", l)
 
-	dinit.ConfigureLog("debug")
+	l = withLogger(t, func() {
+		var in bytes.Buffer
+		var out bytes.Buffer
+		in.WriteString(`test`)
+		dinit.RedirectJSON("run", "test", []byte("test"), io.NopCloser(&in), &out)
 
-	var in bytes.Buffer
-	var out bytes.Buffer
-	in.WriteString(`{"test":"foo"}`)
-	dinit.RedirectJSON("run", "test", []byte("test"), io.NopCloser(&in), &out)
-
-	assert.Regexp(t, `\{"test":"foo","service":test,"stage":"run","logged":".+"\}`, out.String())
-	assert.Equal(t, "", buf.String())
-
-	in.Reset()
-	out.Reset()
-	buf.Reset()
-
-	in.WriteString(`test`)
-	dinit.RedirectJSON("run", "test", []byte("test"), io.NopCloser(&in), &out)
-
-	assert.Equal(t, "", out.String())
-	assert.Regexp(t, `\d+Z dinit: warning: test/run: not JSON stdout: test\n`, buf.String())
+		assert.Equal(t, "", out.String())
+	})
+	assert.Regexp(t, `\d+Z dinit: warning: test/run: not JSON stdout: test\n`, l)
 }
