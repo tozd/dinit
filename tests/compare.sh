dcc22320079c0bf587f6b8e563312d3d1b484e5e
Mitar <mitar.git@tnode.com>
2023-06-30 10:18:21 +0200

    Enable Docker e2e tests.

--- tests/compare.sh
+++ tests/compare.sh
@@ -11,13 +11,25 @@ for file in *.log; do
   sed -E -i 's/[^ "]+Z/Z/g' "$file"
   # We remove timestamps in Unix format.
   sed -E -i 's/: [^:]+[0-9]+:[0-9]+:[0-9]+ UTC .+/: Z/g' "$file"
-  # Sometimes an extra date process is detected, we remove logs about is.
-  sed -E -i '/date\/X/d' "$file"
   # We sort files to remove order differences and remove duplicate lines.
   sort -u -o "$file" "$file"
 done
 
+ret=0
 for file in *.log; do
-  echo "$file"
-  diff "$file" "tests/$file"
+  match=0
+  for result in tests/results/$(basename "$file" .log)-*.log; do
+    if diff "$file" "$result" > /dev/null 2>&1 ; then
+      match=1
+      break
+    fi
+  done
+  if [ "$match" = 0 ]; then
+    echo "$file: ERROR"
+    ret=1
+  else
+    echo "$file: OK"
+  fi
 done
+
+exit $ret
